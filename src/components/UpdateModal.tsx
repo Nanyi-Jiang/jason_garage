22b0b5d8464881b3375b1bfdee81508e4f42dff1
Nanyi Jiang <74779000+Nanyi-Jiang@users.noreply.github.com>
2023-12-01 01:59:03 -0500

    chore: update modal component

---
+++ src/components/UpdateModal.tsx
@@ -0,0 +1,41 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import InputForm, { type FormItem } from "./InputForm";
+
+export const UpdateModal = (props: {
+  tableName: string;
+  formInputs: FormItem[];
+  submitFunction: any;
+  refetch?: any;
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { tableName, formInputs, submitFunction, refetch } = props;
+  return (
+    <>
+      <Button onClick={onOpen}>Update a {tableName} record</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <InputForm
+              formItems={formInputs}
+              submitFunction={submitFunction}
+              onClose={onClose}
+              refetch={refetch}
+            />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
